d6eaa3581cef905682749afff255e23d883ef358
liberjellyfish <2050573754@qq.com>
2025-12-10 20:08:32 +0800

    draw a line,build a square
    从0实现mesh，实现可视化切割线

---
+++ Assets/Scripts/MouseSlicer.cs
@@ -0,0 +1,84 @@
+using System;
+using UnityEngine;
+
+public class MouseSlicer : MonoBehaviour
+{   //起点终点坐标
+    private Vector3 startPoint;
+    private Vector3 endPoint;
+
+    //当前是否在拖拽鼠标
+    private bool isDragging = false;
+
+    //引用组件绘制可见红线
+    private LineRenderer lineVisualizer;
+
+    void Start()
+    {
+        lineVisualizer = GetComponent<LineRenderer>();
+        lineVisualizer.positionCount = 2;//起点终点
+        lineVisualizer.enabled = false;//初始禁用渲染
+
+        lineVisualizer.startWidth = 0.05f;//限制线条宽度
+        lineVisualizer.endWidth = 0.05f;
+    }
+
+    void Update()
+    {
+        //鼠标按下
+        if(Input.GetMouseButtonDown(0))
+        {
+            startPoint = GetWorldMousePosition();
+            isDragging = true;
+            lineVisualizer.enabled = true;
+            //刚按下时起点终点重合
+            lineVisualizer.SetPosition(0, startPoint);
+            lineVisualizer.SetPosition(1, startPoint);
+        }
+
+        //鼠标拖拽中
+        if (isDragging && Input.GetMouseButton(0))
+        {
+            endPoint = GetWorldMousePosition();
+
+            lineVisualizer.SetPosition(1, endPoint);
+        }
+
+        if(isDragging && Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+            lineVisualizer.enabled = false; 
+            endPoint = GetWorldMousePosition();
+
+            PerformSlice(startPoint, endPoint);
+        }
+    }
+    //将鼠标的屏幕坐标(像素)转换为世界坐标
+    private Vector3 GetWorldMousePosition()
+    {
+        Vector3 screenPosition = Input.mousePosition;
+
+
+        float distanceToCamera = -Camera.main.transform.position.z;
+        screenPosition.z = distanceToCamera;
+
+
+        Vector3 worldPos =  Camera.main.ScreenToWorldPoint(screenPosition);
+
+        worldPos.z = -1f;
+
+        return worldPos;
+    }
+
+    private void PerformSlice(Vector3 slicerStart,Vector3 slicerEnd)
+    {
+        //如果太短不切割
+        if (Vector3.Distance(slicerStart, slicerEnd) < 0.1f) 
+        {
+            return;
+        }
+
+        Debug.Log($"[切割指令] Start: {slicerStart} -> End: {slicerEnd}");
+
+        //实施切割算法
+    }
+}
---
+++ Assets/Scripts/SimpleQuadGen.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter),typeof(MeshRenderer))]
+public class SimpleQuadGen : MonoBehaviour
+{
+    void Start()
+    {
+        //定义顶点，顺时针
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(-1,-1, 0),
+            new Vector3(-1, 1, 0),
+            new Vector3( 1, 1, 0),
+            new Vector3( 1,-1, 0)
+        };
+
+        //定义uv（对应顶点的纹理坐标，范围0-1，归一化）
+        Vector2[] uv = new Vector2[]
+        {
+            new Vector2(0,0),
+            new Vector2(0,1),
+            new Vector2(1,1),
+            new Vector2(1,0)
+        };
+
+        //定义三角形索引
+        int[]triangles = new int[]
+        {
+            0,1,2,//第一个三角形
+            0,2,3//第二个三角形
+        };
+        
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+
+        //赋值给组件
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+
+    
+}
